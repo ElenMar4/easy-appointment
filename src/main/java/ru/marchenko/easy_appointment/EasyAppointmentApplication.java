8053b4f0a9ac66c01b7ad0ce07008bfa26cc9b6b
ElenMar4 <mar4enko.elena.jurjevna@gmail.com>
2024-03-14 13:34:56 +0300

    Initial commit

---
+++ src/main/java/ru/marchenko/easy_appointment/EasyAppointmentApplication.java
@@ -0,0 +1,13 @@
+package ru.marchenko.easy_appointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyAppointmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyAppointmentApplication.class, args);
+	}
+
+}
